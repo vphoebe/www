8ea631e074e7c14f7b0cd4fb382e39049cd1b901
Nick Seman <me@vphoebe.dev>
2022-11-13 12:44:46 -0800

    feat: init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/pkmn/:path*",
+        destination: "https://pkmn-tools.vphoebe.dev/pkmn/:path*",
+      },
+      {
+        source: "/kojima/:path*",
+        destination: "https://kojima-ipsum.vphoebe.dev/kojima/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
